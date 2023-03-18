ada1f2e9e8f121e1fb879dea49b382dcdad72211
Sexure <sexurexion@gmail.com>
2023-03-18 14:39:46 +0000

    define literals

---
+++ Kotlin-Programming-Language-Overview/examples/datatypes/literals.kt
@@ -0,0 +1,42 @@
+fun main() {
+    // integer literals
+    val intDecimal = 100
+    val intHexadecimal = 0x64
+    val intBinary = 0b1100100
+    val intLong = 100L
+
+    // floating point literals
+    val float1 = 1.23f
+    val float2 = 1.23F
+    val double1 = 1.23
+    val double2 = 1.23d
+    val double3 = 1.23D
+    val double4 = 1.23e10
+    val double5 = 1.23E10
+
+    // character literals
+    val char1 = 'a'
+    val char2 = '\n'
+    val char3 = '\u0061'
+
+    // boolean literals
+    val trueLiteral = true
+    val falseLiteral = false
+
+    println(intDecimal)
+    println(intHexadecimal)
+    println(intBinary)
+    println(intLong)
+    println(float1)
+    println(float2)
+    println(double1)
+    println(double2)
+    println(double3)
+    println(double4)
+    println(double5)
+    println(char1)
+    println(char2)
+    println(char3)
+    println(trueLiteral)
+    println(falseLiteral)
+}
