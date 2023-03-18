56454dc1b4c27d33078b5abfe9b79d8e470f4821
Sexure <sexurexion@gmail.com>
2023-03-18 14:47:27 +0000

    templates

---
+++ Kotlin-Programming-Language-Overview/examples/datatypes/templates.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val name = "John"
+    val age = 30
+    val job = "Developer"
+
+    val message1 = "My name is $name and I am $age years old"
+    val message2 = "I work as a $job"
+
+    println(message1)
+    println(message2)
+}
