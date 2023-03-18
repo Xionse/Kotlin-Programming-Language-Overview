d8f3bcf9a2be19d427509595dc27e2747b99d024
Sexure <sexurexion@gmail.com>
2023-03-18 15:02:54 +0000

    define Exceptions

---
+++ Kotlin-Programming-Language-Overview/examples/error_handling/exceptions.kt
@@ -0,0 +1,12 @@
+fun main() {
+    try {
+        val num1 = 10
+        val num2 = 0
+        val result = num1 / num2
+        println("Result: $result")
+    } catch (e: ArithmeticException) {
+        println("Cannot divide by zero!")
+    } finally {
+        println("End of program")
+    }
+}
