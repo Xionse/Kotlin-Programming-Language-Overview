edfc06d364c43aa0b57f62fed05169f364844a37
Sexure <sexurexion@gmail.com>
2023-03-20 13:34:29 +0000

    Add literals

--- Kotlin-Programming-Language-Overview/examples/datatypes/literals.kt
+++ Kotlin-Programming-Language-Overview/examples/datatypes/literals.kt
@@ -1,44 +1,21 @@
 fun main() {
-    // integer literals
-    val intDecimal = 100
-    val intHexadecimal = 0x64
-    val intBinary = 0b1100100
-    val intLong = 100L
+    // integer literal
+    val number = 42
+    println("The number is $number")
 
-    // floating point literals
-    val float1 = 1.23f
-    val float2 = 1.23F
-    val double1 = 1.23
-    val double2 = 1.23d
-    val double3 = 1.23D
-    val double4 = 1.23e10
-    val double5 = 1.23E10
+    // floating-point literal
+    val pi = 3.14
+    println("The value of pi is $pi")
 
-    // character literals
-    val char1 = 'a'
-    val char2 = '\n'
-    val char3 = '\u0061'
+    // boolean literal
+    val isTrue = true
+    println("The value of isTrue is $isTrue")
 
-    // boolean literals
-    val trueLiteral = true
-    val falseLiteral = false
+    // character literal
+    val letter = 'A'
+    println("The letter is $letter")
 
-    println(intDecimal)
-    println(intHexadecimal)
-    println(intBinary)
-    println(intLong)
-    println(float1)
-    println(float2)
-    println(double1)
-    println(double2)
-    println(double3)
-    println(double4)
-    println(double5)
-    println(char1)
-    println(char2)
-    println(char3)
-    println(trueLiteral)
-    println(falseLiteral)
+    // string literal
+    val message = "Hello, world!"
+    println(message)
 }
-
-
