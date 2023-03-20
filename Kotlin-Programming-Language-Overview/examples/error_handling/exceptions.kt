01a964603130f1f95957444ab8ad21989bcbcadd
Sexure <sexurexion@gmail.com>
2023-03-20 13:19:37 +0000

    Add the exception  types

--- Kotlin-Programming-Language-Overview/examples/error_handling/exceptions.kt
+++ Kotlin-Programming-Language-Overview/examples/error_handling/exceptions.kt
@@ -17,3 +17,55 @@ fun main() {
     val result = divide(10, 2)
     println("Result: $result")
 }
+
+
+
+/* OTHER TYPES OF EXCEPTIONS */
+
+
+// Arithmetic Exceptions 
+
+try {
+    // arithmetic operation
+} catch (e: ArithmeticException) {
+    // handle the exception
+}
+
+// Nullpoointer Exceptions 
+
+val nullableString: String? = null
+val length = nullableString?.length ?: 0
+
+
+
+
+
+
+// Index OutofBounds 
+
+val array = arrayOf(1, 2, 3)
+try {
+    val element = array[5]
+} catch (e: IndexOutOfBoundsException) {
+    // handle the exception
+}
+
+val index = 5
+if (index in 0 until array.size) {
+    val element = array[index]
+} else {
+    // handle the exception
+}
+
+
+
+
+//IOException 
+
+val file = File("path/to/file")
+try {
+    file.readText()
+} catch (e: IOException) {
+    // handle the exception
+}
+
