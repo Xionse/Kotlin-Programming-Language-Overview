d8039104aa10c31e2b1cf13afe4c0666bb8a3db8
Sexure <sexurexion@gmail.com>
2023-03-20 13:27:07 +0000

    Add other Implementations

--- Kotlin-Programming-Language-Overview/examples/error_handling/null_safety.kt
+++ Kotlin-Programming-Language-Overview/examples/error_handling/null_safety.kt
@@ -1,3 +1,25 @@
+fun printLength(text: String?) {
+    if (text != null) {
+        // use the safe call operator to get the length of the string
+        println("Length of '$text': ${text.length}")
+    } else {
+        println("Text is null")
+    }
+}
+
+fun main() {
+    // call the printLength function with a non-null string
+    printLength("Hello, world!")
+
+    // call the printLength function with a null string
+    printLength(null)
+}
+
+
+
+
+// Other examples 
+
 fun main() {
     var name: String? = "John"
     name = null
