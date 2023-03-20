0c1da0a076fdc3aea7b30f27d971d087db76e826
Sexure <sexurexion@gmail.com>
2023-03-20 13:52:28 +0000

    basic Control statemnts

--- Kotlin-Programming-Language-Overview/examples/basic/control_flow.kt
+++ Kotlin-Programming-Language-Overview/examples/basic/control_flow.kt
@@ -22,3 +22,42 @@ fun main() {
         println(i)
     }
 }
+
+
+
+
+
+
+// alternatively 
+
+
+fun main() {
+    val num = 5
+
+    // if-else statement to determine if a number is odd or even
+    if (num % 2 == 0) {
+        println("$num is even")
+    } else {
+        println("$num is odd")
+    }
+
+    // when statement to determine the type of a variable
+    val x: Any = "Hello"
+    when (x) {
+        is Int -> println("$x is an integer")
+        is String -> println("$x is a string")
+        else -> println("$x is not an integer or a string")
+    }
+
+    // for loop to iterate over a range of numbers
+    for (i in 1..5) {
+        println(i)
+    }
+
+    // while loop to repeat a block of code until a condition is met
+    var i = 1
+    while (i <= 5) {
+        println(i)
+        i++
+    }
+}
